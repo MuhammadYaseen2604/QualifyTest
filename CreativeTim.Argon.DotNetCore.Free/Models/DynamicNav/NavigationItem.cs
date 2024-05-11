0bdfa957a19e014c9e2df6289189441c94968a42
DESKTOP-IH65KU4\Yaseen <12345666.my@gmail.com>
2024-05-11 20:47:09 +0500

    many to many Relation Navigation items

--- CreativeTim.Argon.DotNetCore.Free/Models/DynamicNav/NavigationItem.cs
+++ CreativeTim.Argon.DotNetCore.Free/Models/DynamicNav/NavigationItem.cs
@@ -1,18 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CreativeTim.Argon.DotNetCore.Free.Models.DynamicNav
 {
     public class NavigationItem
-    {
+    {   [Key]
         public int Id { get; set; }
-        public int? ParentId { get; set; }
+
+        public int? ParentId{ get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
-        //public string Url { get; set; }
         public int Order { get; set; }
 
-        public NavigationItem Parent { get; set; }
-
-        //public int? ChildId { get; set; }
-        public NavigationItem Child { get; set; }
+        // Navigation properties for Many-to-Many relationship
+        [ForeignKey("ParentId")]
+        public ICollection<NavigationItem> ParentNavigation { get; set; }
 
 
     }
